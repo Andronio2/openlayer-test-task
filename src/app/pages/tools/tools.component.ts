0887253836636191ea5361f5132df24198efb05d
Andronio <gprsboost03@mail.ru>
2025-05-29 14:32:51 +0500

    tools

--- src/app/pages/tools/tools.component.ts
+++ src/app/pages/tools/tools.component.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PopoverModule } from 'primeng/popover';
 
 @Component({
   selector: 'app-tools',
   templateUrl: './tools.component.html',
-  // styles: [':host{display:contents}'], // Makes component host as if it was not there, can offer less css headaches. Use @HostBinding class approach for easier overrides.
-  // host: { class: 'contents' },
-  imports: [CommonModule],
+  styleUrls: ['./tools.component.scss'],
+  imports: [CommonModule, PopoverModule],
 })
-export class ToolsComponent {
-  // @HostBinding('class') protected readonly class = 'contents'; // Makes component host as if it was not there, can offer less css headaches. Assumes .contents{display:contents} css class exits
-  // constructor() {}
-}
+export class ToolsComponent {}
